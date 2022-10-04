21b8ccfe1b05f560118ac93d3aa252d6af2d84f0
Okky Maheswara <okkymahes@gmail.com>
2022-10-04 14:49:20 +0800

    no js

--- assets/script.js
+++ assets/script.js
@@ -1,2 +1,18 @@
-console.log("Selamat Anda berhasil menggunakan JavaScript pada Website");
+// console.log("Selamat Anda berhasil menggunakan JavaScript pada Website");
 // alert("Hello Browser!")
+
+document.getElementsByClassName("toggle-navbar")[0].addEventListener("click", toogleClass);
+
+function toogleClass() {
+  document.getElementsByClassName("hamburger-menu")[0].classList.toggle('open')
+  document.getElementsByClassName("menu")[0].classList.toggle('open')
+}
+
+function myFunction() {
+  var x = document.getElementById("menu");
+  if (x.style.display === "block") {
+    x.style.display = "none";
+  } else {
+    x.style.display = "block";
+  }
+}
